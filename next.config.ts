2ac374457582ab4ddd919ea22bce5325a7b96e9a
saniashaukat23 <saniashaukat9@example.com>
2025-11-18 22:31:08 +0500

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  // Keep any other config you already had here, like images or env
 };
 
-export default nextConfig;
+module.exports = nextConfig;
