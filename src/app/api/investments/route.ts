9b01250359f1653f6012cd544f63ffa3ebdbd83e
saniashaukat23 <saniashaukat9@example.com>
2025-11-18 21:29:26 +0500

    Initial commit

--- src/app/api/investments/route.ts
+++ src/app/api/investments/route.ts
@@ -1,13 +1,13 @@
-import { auth } from "@/auth";
+// import { auth } from "@/auth";
 import { dbConnect } from "@/lib/dbConnect";
-import Investment from "@/models/investment";
+import Investment from "@/models/Investment";
 
 export async function GET() {
-  const session = await auth();
-  if (!session?.user) return new Response("Unauthorized", { status: 401 });
-  await dbConnect();
-  const data = await Investment.find({ userId: (session.user as any).id }).sort(
-    { createdAt: -1 }
-  );
-  return Response.json({ data });
+  // const session = await auth();
+  // if (!session?.user) return new Response("Unauthorized", { status: 401 });
+  // await dbConnect();
+  // const data = await Investment.find({ userId: (session.user as any).id }).sort(
+  //   { createdAt: -1 }
+  // );
+  // return Response.json({ data });
 }
